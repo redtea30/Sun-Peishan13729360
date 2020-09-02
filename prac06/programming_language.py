75d072f9647ba0a570cedb2ccc04f52f1c2e820f
redtea30 <58327220+redtea30@users.noreply.github.com>
2020-09-02 22:46:56 +0800

    Create programming_language.py

---
+++ prac06/programming_language.py
@@ -0,0 +1,25 @@
+def main():
+
+
+    class ProgrammingLanguage:
+        """Represent a Programming Language object"""
+
+        def __init__(self, language_name = "", typing="", reflection="", year=''):
+            self.language_name = language_name
+            self.typing = typing
+            self.reflection = reflection
+            self.year = year
+
+        def is_dynamic(self):
+            return self.reflection
+
+        def __str__(self):
+            return '{0}, {1} typing, reflection={2}, first appeared in {3}'.format(self.language_name, self.typing,
+                                                                                   self.reflection, self.year)
+
+        """ Printing the list """
+        def __repr__(self):
+            return '{0}, {1} typing, reflection={2}, first appeared in {3}'.format(self.language_name, self.typing,
+                                                                                   self.reflection, self.year)
+
+main()
