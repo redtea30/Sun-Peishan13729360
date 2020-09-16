7ca2dcc3767686f154414bfb4d3915de78279a91
redtea30 <58327220+redtea30@users.noreply.github.com>
2020-09-16 22:17:54 +0800

    Update taxi_test.py

--- prac08/taxi_test.py
+++ prac08/taxi_test.py
@@ -1,1 +1,18 @@
+from taxi import Taxi
 
+
+def main():
+
+    my_taxi = Taxi("Prius 1", 100)
+    my_taxi.drive(40)
+    print(my_taxi)
+
+    my_fare = my_taxi.get_fare()
+    print(my_fare)
+
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
